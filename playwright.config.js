d1dabaac8ab4ea60864763ac9ebe48d97f7015dd
Egor <egor.egorov0004@gmail.com>
2025-07-15 22:56:02 +0200

    fix #34

--- playwright.config.js
+++ playwright.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
-  webServer: {
-    command: 'make start',
-    url: 'http://localhost:5002',
-    timeout: 60000,
-    reuseExistingServer: true,
-  },
-  use: {
-    baseURL: 'http://localhost:5002',
-  },
+    webServer: {
+      command: 'npm run start-frontend',
+      url: 'http://localhost:5002',
+      timeout: 60000,
+      reuseExistingServer: true,
+    },
+    use: {
+      baseURL: 'http://localhost:5002',
+    },
 });
+  
